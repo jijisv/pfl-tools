b50772b9d3276be4f2f6be5b0208de153cd6a99d
jijisv@gmail.com <jijisv@gmail.com@4626ab57-8642-4551-16b0-378a1a38676d>
2013-07-29 11:45:59 +0000

    programmingforliving.com offline blog - used to preview the blog entries

---
+++ trunk/pfl-offline-blog/OfflineBlogServer.py
@@ -0,0 +1,52 @@
+'''
+OfflineBlogServer
+
+Created on Oct 12, 2012
+@author: Jiji_Sasidharan
+'''
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import os
+import sys
+import urllib
+
+class OfflineBlogRequestHandler(SimpleHTTPRequestHandler):
+    
+    def do_GET(self):
+        pr = urllib.parse.urlparse(self.path)
+        if pr.path == '/v':
+            params = urllib.parse.parse_qs(pr.query)
+            self.createFile(pr.path, params['p'][0])
+        SimpleHTTPRequestHandler.do_GET(self)
+    
+    def createFile(self, path, post_name):
+        template = open(os.path.join(os.getcwd(), 'index.htm'), 'r').read()
+        post = open(os.path.join(os.getcwd(), 'drafts', post_name + '.html'), 'r').read().strip()
+
+        start = post.find('<h1>')
+        if (start == 0):
+            end = post.find('</h1>')
+            post_title = post[:end]
+            post = post[end + 5:]
+        else :
+            post_title = post_name    
+
+        f = open(os.path.join(os.getcwd(), 'v'), 'w')
+        f.write(template.replace('$BLOG_ENTRY_BODY$', post).replace('$BLOG_ENTRY_TITLE$', post_title))
+        f.close()
+
+    def guess_type(self, path):
+        if path.endswith('/v'):
+            return 'text/html'
+        else: 
+            SimpleHTTPRequestHandler.guess_type(self, path);
+
+if __name__ == '__main__':
+    httpd = HTTPServer(('', int(sys.argv[1])), OfflineBlogRequestHandler)
+    try:
+        print("Serving HTTP on", httpd.server_address , "port", httpd.server_port, "...")
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+        sys.exit()
+        
